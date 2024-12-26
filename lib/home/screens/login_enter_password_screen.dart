8520c5ed03977faf0225045c1b9e0c3ac64c7fe4
OneStarSevenAzar <onestarsevenazar@gmail.com>
2024-12-26 18:04:06 +0330

    2 full screen

--- lib/home/screens/login_enter_password_screen.dart
+++ lib/home/screens/login_enter_password_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shop_style/home/screens/basic_information_screen.dart';
+import 'package:shop_style/home/widgets/OTPWidget.dart';
+import 'package:shop_style/home/widgets/custom_button.dart';
+import 'package:shop_style/home/widgets/custom_text.dart';
 
 class LoginEnterPasswordScreen extends StatefulWidget {
   const LoginEnterPasswordScreen({super.key});
@@ -10,15 +13,6 @@ class LoginEnterPasswordScreen extends StatefulWidget {
 }
 
 class _LoginEnterPasswordScreenState extends State<LoginEnterPasswordScreen> {
-  TextEditingController controller1 = TextEditingController();
-  TextEditingController controller2 = TextEditingController();
-  TextEditingController controller3 = TextEditingController();
-  TextEditingController controller4 = TextEditingController();
-  FocusNode focusNode1 = FocusNode();
-  FocusNode focusNode2 = FocusNode();
-  FocusNode focusNode3 = FocusNode();
-  FocusNode focusNode4 = FocusNode();
-
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
@@ -30,82 +24,44 @@ class _LoginEnterPasswordScreenState extends State<LoginEnterPasswordScreen> {
         padding: EdgeInsets.symmetric(horizontal: width / 15),
         child: Directionality(
           textDirection: TextDirection.rtl,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(height: height / 10),
-              const Text(
-                'کد تایید را وارد کنید',
-                style: TextStyle(
-                  color: Color(0xFF0D1619),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverToBoxAdapter(
+                child: CustomText(
+                  text: 'کد تایید را وارد کنید',
+                  topPadding: height / 10,
+                  theme: Theme.of(context).textTheme.titleLarge,
                 ),
               ),
-              SizedBox(height: height / 100),
-              const Text(
-                'کد تایید برای شماره 09120000000 پیامک شد.',
-                style: TextStyle(
-                  color: Color(0xFF000080),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14,
+              SliverToBoxAdapter(
+                child: CustomText(
+                  text: 'کد تایید برای شماره 09120000000 پیامک شد.',
+                  topPadding: height / 100,
+                  theme: Theme.of(context).textTheme.bodySmall,
                 ),
               ),
-              SizedBox(height: height / 50),
-              Container(
-                height: height / 20,
-                decoration: BoxDecoration(
-                  border: Border.all(color: const Color(0xFFE5E5E5), width: 2),
-                  color: const Color(0xFFFFFFFF),
-                  borderRadius: const BorderRadius.all(Radius.circular(360)),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width / 40),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      oneNumberCode(
-                          width, controller1, focusNode1, context, null),
-                      oneNumberCode(
-                          width, controller2, focusNode2, context, focusNode1),
-                      oneNumberCode(
-                          width, controller3, focusNode3, context, focusNode2),
-                      oneNumberCode(
-                          width, controller4, focusNode4, context, focusNode3),
-                    ],
-                  ),
+              SliverToBoxAdapter(
+                child: OTPWidget(topPadding: height / 50),
+              ),
+              SliverToBoxAdapter(
+                child: CustomText(
+                  text: 'ارسال مجدد کد تایید تا 2:41 ثانیه دیگر',
+                  textAlign: TextAlign.center,
+                  topPadding: height / 3.6,
+                  theme: Theme.of(context).textTheme.bodySmall,
                 ),
               ),
-              SizedBox(height: height / 3.6),
-              const Text(
-                textAlign: TextAlign.center,
-                'ارسال مجدد کد تایید تا 2:41 ثانیه دیگر',
-                style: TextStyle(
-                  color: Color(0xFF000080),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14,
-                ),
-              ),
-              SizedBox(height: height / 30),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const BasicInformationScreen(),
-                    ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF0D1619),
-                  minimumSize: Size(width / 3, height / 20),
-                ),
-                child: const Text(
-                  'ورود / ثبت نام',
-                  style: TextStyle(
-                    color: Color(0xFFFFFFFF),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
+              SliverToBoxAdapter(
+                child: CustomButton(
+                  textButton: 'ورود / ثبت نام',
+                  topPadding: height / 30,
+                  onClick: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const BasicInformationScreen(),
+                      ),
+                    );
+                  },
                 ),
               ),
             ],
@@ -114,41 +70,4 @@ class _LoginEnterPasswordScreenState extends State<LoginEnterPasswordScreen> {
       ),
     );
   }
-}
-
-Widget oneNumberCode(
-  double width,
-  TextEditingController controller,
-  FocusNode focusNode,
-  BuildContext context,
-  FocusNode? previousFocusNode,
-) {
-  return SizedBox(
-    width: width / 20,
-    child: TextField(
-      focusNode: focusNode,
-      keyboardType: TextInputType.number,
-      controller: controller,
-      maxLength: 1,
-      textAlign: TextAlign.center,
-      decoration: const InputDecoration(
-        counterText: ' ',
-        border: InputBorder.none,
-        hintText: '_',
-        hintStyle: TextStyle(
-          color: Color(0xFF000000),
-          fontSize: 18,
-        ),
-      ),
-      onChanged: (value) {
-        if (value.length == 1) {
-          FocusScope.of(context).nextFocus();
-        } else if (value.isEmpty) {
-          if (previousFocusNode != null) {
-            FocusScope.of(context).requestFocus(previousFocusNode);
-          }
-        }
-      },
-    ),
-  );
 }
